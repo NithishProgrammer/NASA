76b1ae4bfb9ce7c1d42def9b867cbc3900df516b
Nithish S <150901506+NithishProgrammer@users.noreply.github.com>
2024-07-03 21:54:21 +0530

    Add files via upload

---
+++ Newsapp/app.js
@@ -0,0 +1,37 @@
+let api = "https://newsdata.io/api/1/latest?apikey=pub_47852a84e3103db1cc35655fa199918e20ae7&language=en";
+let title = document.getElementById('title');
+let content = document.getElementById('content');
+let writer = document.getElementById('writer');
+let date = document.getElementById('date');
+let article = document.getElementById('article');
+let image = document.getElementById('image')
+
+function search() {
+	let searchQuery = document.getElementById('search').value;
+	let query = `${api}&q=${searchQuery}`;
+	fetch(query).then((response) => {
+		return response.json();
+	}).then((data) => {
+		console.log(data);
+		if (data.results && data.results.length > 1) {
+			let firstArticle = data.results[2];
+			title.innerText = firstArticle.title;
+			content.innerText = firstArticle.content;
+			writer.innerText = firstArticle.creator;
+			date.innerText = firstArticle.pubDate;
+			article.innerText = firstArticle.description;
+			image.innerText = firstArticle.image_url;
+		} else {
+			title.innerText = "No results found";
+			content.innerText = "";
+			writer.innerText = "";
+			date.innerText = "";
+			article.innerText = "";
+		}
+	}).catch((error) => {
+		console.error('Error fetching data:', error);
+	});
+
+
+ 
+}
